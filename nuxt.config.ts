dfa2f903b7a1fc8122266ed0c4e7b676fc74fa8a
Lucas Bigeard <luc.allau@gmail.com>
2025-11-26 03:48:06 +0100

    replace use of google fonts by nuxt fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    runtimeConfig: {
-        // clé privée (sera accessible seulement côté serveur via useRuntimeConfig())
-        githubToken: process.env.GITHUB_API_KEY || "",
-        gitlabToken: process.env.GITLAB_API_KEY || "",
-        // tout ce qui est sous "public" sera accessible côté client et serveur
-        public: {
-            githubUser: process.env.NUXT_PUBLIC_GITHUB_USER || "LucWaw",
-            gitlabUser: process.env.NUXT_PUBLIC_GITLAB_USER || "LBigeard",
-        },
-    },
-    compatibilityDate: "2025-07-15",
-    devtools: { enabled: true },
+  runtimeConfig: {
+      // clé privée (sera accessible seulement côté serveur via useRuntimeConfig())
+      githubToken: process.env.GITHUB_API_KEY || "",
+      gitlabToken: process.env.GITLAB_API_KEY || "",
+      // tout ce qui est sous "public" sera accessible côté client et serveur
+      public: {
+          githubUser: process.env.NUXT_PUBLIC_GITHUB_USER || "LucWaw",
+          gitlabUser: process.env.NUXT_PUBLIC_GITLAB_USER || "LBigeard",
+      },
+  },
+
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/fonts"],
 });
